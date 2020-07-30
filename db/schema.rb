16d987307d253e969a5b34b31f77d2582a16005a
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-07-30 18:53:25 +0900

    add upload image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_032030) do
+ActiveRecord::Schema.define(version: 2020_07_30_093855) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_032030) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "category_id"
+    t.text "image"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
